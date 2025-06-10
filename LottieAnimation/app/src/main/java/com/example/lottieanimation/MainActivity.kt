1393d93e50ffbbd4451b3767a68d21fa1e23a4a5
kishanghosh090 <kishanghosh090@gmail.com>
2025-06-10 22:22:09 +0530

    android learing

--- LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
+++ LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+
+import androidx.compose.foundation.layout.size
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.lottieanimation.ui.theme.LottieAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LottieAnimationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Lottie()
+//                }
             }
         }
     }
 }
-
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Lottie(){
+    val state by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.welcome)
     )
-}
+    val progress by animateLottieCompositionAsState(
+        state,
+        iterations = LottieConstants.IterateForever
+    )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LottieAnimationTheme {
-        Greeting("Android")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        LottieAnimation(
+            composition = state,
+            progress = { progress },
+
+            modifier = Modifier.align(Alignment.Center).size(200.dp)
+        )
     }
 }
