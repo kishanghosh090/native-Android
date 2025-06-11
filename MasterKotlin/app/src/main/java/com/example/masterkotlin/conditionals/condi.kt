b404302f1eef418acc896c57f18800bc5a014ff8
kishanghosh090 <kishanghosh090@gmail.com>
2025-06-11 19:27:52 +0530

    kotlin conditionals learning

--- MasterKotlin/app/src/main/java/com/example/masterkotlin/conditionals/condi.kt
+++ MasterKotlin/app/src/main/java/com/example/masterkotlin/conditionals/condi.kt
@@ -3,3 +3,19 @@ package com.example.masterkotlin.conditionals
 // conditional statements
 
 // if and else
+fun main(){
+    var isTea: Boolean = true
+    isTea = false
+    if(isTea){
+        println("hello from chai")
+    }else{
+        println("hello from akash")
+    }
+
+    var myNumber: Int = 10
+    when(myNumber){
+        1 -> println("hello from 1")
+        2 -> println("hello from 2")
+        3 -> println("hello from 3")
+    }
+}
