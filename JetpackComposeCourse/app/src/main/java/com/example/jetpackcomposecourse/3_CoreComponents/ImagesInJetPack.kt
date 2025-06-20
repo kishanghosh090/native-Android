14ced2f414d920b8025533338823e67bff91c087
kishanghosh090 <kishanghosh090@gmail.com>
2025-06-21 00:41:09 +0530

    jetpack compose learning upto hanlde image

--- JetpackComposeCourse/app/src/main/java/com/example/jetpackcomposecourse/3_CoreComponents/ImagesInJetPack.kt
+++ JetpackComposeCourse/app/src/main/java/com/example/jetpackcomposecourse/3_CoreComponents/ImagesInJetPack.kt
@@ -1,1 +1,64 @@
 package com.example.jetpackcomposecourse.`3_CoreComponents`
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposecourse.R
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun ImageHandle(){
+    val rainbowColors = remember {
+        Brush.sweepGradient(
+            listOf(
+                Color(0xFF9575CD),
+                Color(0xFFBA68C8),
+                Color(0xFFE57373),
+                Color(0xFFFFB74D),
+                Color(0xFFFFF176),
+                Color(0xFFAED581),
+                Color(0xFF4DD0E1),
+                Color(0xFF9575CD)
+            )
+        )
+    }
+    val borderWidth = 10.dp
+
+    Column(
+        modifier = Modifier.fillMaxSize().fillMaxWidth(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+    ) {
+            Image(
+                painter = painterResource(R.drawable.hiteshsir),
+                contentDescription = "This is menu icon",
+                modifier = Modifier.border(BorderStroke(borderWidth, rainbowColors)),
+                alignment = Alignment.Center,
+
+                )
+
+    }
+}
