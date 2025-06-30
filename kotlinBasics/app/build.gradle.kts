710b22239ddd91d234dd7acf81bc240d17472217
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-07-01 02:28:57 +0530

    kotlin coroutines

--- kotlinBasics/app/build.gradle.kts
+++ kotlinBasics/app/build.gradle.kts
@@ -44,5 +44,19 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    // LifecycleScope (for activities/fragments)
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    // LiveData (optional but often used)
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+    // Coroutines core
+    implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+// Coroutines for Android (Dispatchers.Main etc.)
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
 }
--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/coroutines/Courotines.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/coroutines/Courotines.kt
@@ -1,8 +1,10 @@
 package com.example.kotlinbasics.coroutines
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 suspend fun fetchQuote1(): String {
     delay(2000)
@@ -14,13 +16,36 @@ suspend fun fetchQuote2(): String {
     return "Stay hungry, stay foolish."
 }
 
-fun main() = runBlocking {
-    val q1 = async { fetchQuote1() }
-    val q2 = async { fetchQuote2() }
+ fun main()= runBlocking  {
+//    val q1 = async { fetchQuote1() }
+//    val q2 = async { fetchQuote2() }
+//
+//    println("Quotes:")
+//    println("1: ${q1.await()}")
+//    println("2: ${q2.await()}")
+//
+//    println("End of program")
+    println("Start: ${Thread.currentThread().name}")
+    withContext (Dispatchers.IO) {
+        val quote = fetchQuote1()
+        println("Fetched on IO thread: $quote ${Thread.currentThread().name}")
+    }
+     withContext (Dispatchers.IO) {
+         val quote = fetchQuote1()
+         println("Fetched on IO thread: $quote ${Thread.currentThread().name}")
+     }
+     withContext (Dispatchers.IO) {
+         val quote = fetchQuote1()
+         println("Fetched on IO thread: $quote ${Thread.currentThread().name}")
+     }
+     withContext (Dispatchers.IO) {
+         val quote = fetchQuote1()
+         println("Fetched on IO thread: $quote ${Thread.currentThread().name}")
+     }
 
-    println("Quotes:")
-    println("1: ${q1.await()}")
-    println("2: ${q2.await()}")
+     println("End: ${Thread.currentThread().name}")
 
-    println("End of program")
+
 }
+
+
