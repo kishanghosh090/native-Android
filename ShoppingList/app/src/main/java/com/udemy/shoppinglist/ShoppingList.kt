478c7b162d4f7df25fa8d1eec606a084cba6bb38
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-07-02 00:11:03 +0530

    shopping added

--- ShoppingList/app/src/main/java/com/udemy/shoppinglist/ShoppingList.kt
+++ ShoppingList/app/src/main/java/com/udemy/shoppinglist/ShoppingList.kt
@@ -1,18 +1,28 @@
 package com.udemy.shoppinglist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -23,7 +33,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+
 
 
 data class ShoppingItem(
@@ -43,9 +55,12 @@ fun ShoppingListApp(
     ) }
     var showDialog by remember { mutableStateOf(false) }
 
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("1") }
+
     Column(
         modifier = Modifier.fillMaxSize().padding(innerPadding),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ){
         FloatingActionButton (
             onClick = {
@@ -68,8 +83,22 @@ fun ShoppingListApp(
                 .fillMaxSize()
                 .padding(16.dp)
         ){
-            items(sItems){
-
+            items(sItems){ item ->
+                ShoppingListItem(
+                    item,
+                    onEdit = {
+                        sItems = sItems.map {
+                            if (it.id == item.id) {
+                                it.copy(isEditing = true)
+                            } else {
+                                it.copy(isEditing = false)
+                            }
+                        }
+                    },
+                    onDelete = { id ->
+                        sItems = sItems.filter { it.id != id }
+                    }
+                )
             }
         }
     }
@@ -77,29 +106,56 @@ fun ShoppingListApp(
 
     if (showDialog){
         AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text(text = "Add Shopping Item") },
+            modifier = Modifier
+                .border(
+                    color = Color.Green,
+                    width = 1.dp,
+                    shape = AlertDialogDefaults.shape
+                ),
+            onDismissRequest = {
+                showDialog = false
+            },
+            title = {
+                Text(text = "Add Shopping Item")
+            },
             text = {
                 Column {
                     OutlinedTextField(
-                        value = "",
-                        onValueChange = { /*TODO*/ },
+                        value = itemName,
+                        onValueChange = {
+                            itemName = it
+                        },
                         label = { Text(text = "Item Name") },
                         singleLine = true,
-                        modifier = Modifier.padding(vertical = 8.dp)
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        shape = RoundedCornerShape(10.dp)
+
                     )
                     OutlinedTextField(
-                        value = "",
-                        onValueChange = { /*TODO*/ },
+                        value = itemQuantity,
+                        onValueChange = {
+                            itemQuantity = it
+                        },
                         label = { Text(text = "Quantity") },
                         singleLine = true,
-                        modifier = Modifier.padding(vertical = 8.dp)
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        shape = RoundedCornerShape(10.dp)
                     )
                 }
             },
             confirmButton = {
                 Button(
                     onClick = {
+                        if (itemName.isNotEmpty()){
+                            val item = ShoppingItem(
+                                id = sItems.size + 1,
+                                name = itemName,
+                                quantity = itemQuantity.toIntOrNull() ?: 0
+                            )
+                            sItems = sItems + item
+                        }
+                        itemName = ""
+                        itemQuantity = "1"
                         showDialog = false
                     }
                 ) {
@@ -115,7 +171,62 @@ fun ShoppingListApp(
                     Text(text = "Cancel")
                 }
             },
+
         )
     }
 
+
+
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEdit: () -> Unit,
+    onDelete: (id: Int) -> Unit
+){
+
+    var randomColorNumber = (0..4).random()
+    var randomColor = Color.Red
+    when(randomColorNumber){
+        0 -> randomColor = Color.Red
+        1 -> randomColor = Color.Green
+        2 -> randomColor = Color.Blue
+        3 -> randomColor = Color.Yellow
+        4 -> randomColor = Color.Magenta
+    }
+    Box(
+        modifier = Modifier.padding(vertical = 8.dp).border(
+            border = BorderStroke(2.dp, randomColor),
+            shape = RoundedCornerShape(10.dp)
+        ),
+    ){
+        Row (
+            modifier = Modifier.padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+
+            Text(
+                text = item.name,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.weight(1f).padding(start = 8.dp)
+            )
+            Text(
+                text = item.quantity.toString(),
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+
+            IconButton (onClick = onEdit) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton (onClick = { onDelete(item.id) }) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+
+
+
+        }
+    }
 }
