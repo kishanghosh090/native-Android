f608b0994607d1f2dabad1da0fe4c9a0635c5675
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-05 21:16:22 +0530

    add adv kotlin

---
+++ AdvanceKotlin/src/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    println("hello from chai");
+    val name: String = "kishan"
+    var age = 20
+    if (age>18){
+        println(name)
+    }else{
+        println("not vote")
+    }
+
+
+}
---
+++ COMPOSEMUSICAPP/app/src/main/java/com/example/composemusicapp/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.composemusicapp
+
+class MainViewModel {
+}
