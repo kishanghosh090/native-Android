e79e7b4fe7e2c2c9c763c14adb07290cf9ac5720
kishanghosh090 <kishanghosh090@gmail.com>
2025-10-05 10:38:13 +0530

    add data types

---
+++ second-phase/kotlinRiviosn/src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+package org.example
+
+fun main() {
+    var name = "kishan"
+    println("hello from $name")
+
+    name = readln()
+
+
+    if(name.length >5){
+        println("hello can you need tea")
+    }else{
+        println("hello do you need coffee")
+    }
+
+
+    println("hello from kishan")
+    return
+}
---
+++ second-phase/kotlinRiviosn/src/main/kotlin/basic/Main.kt
@@ -0,0 +1,12 @@
+package org.example.basic
+
+fun main(){
+    println("hello form chai code")
+
+    var name = "kishan"
+    val PI: Float = 3.12f
+    // val is changeable var is not
+    println("my name is $name and PI is equal to $PI")
+    val isActive: Boolean = true
+
+}
