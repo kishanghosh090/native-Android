9ab7367eae43b6adb361185476430ac812b78364
kishanghosh090 <kishanghosh090@gmail.com>
2025-10-05 11:22:10 +0530

    add tointornull

--- second-phase/kotlinRiviosn/src/main/kotlin/basic/Main.kt
+++ second-phase/kotlinRiviosn/src/main/kotlin/basic/Main.kt
@@ -6,7 +6,14 @@ fun main(){
     var name = "kishan"
     val PI: Float = 3.12f
     // val is changeable var is not
-    println("my name is $name and PI is equal to $PI")
+    println("my name is ${name+name} and PI is equal to $PI")
     val isActive: Boolean = true
+
+
+//    val input = readln().toIntOrNull() ?: 0
+    val input = readln().toIntOrNull()?.inc() ///inc exe if readln not NULL
+    input.let {
+        println(input!! % 2 == 0)
+    }
 
 }
