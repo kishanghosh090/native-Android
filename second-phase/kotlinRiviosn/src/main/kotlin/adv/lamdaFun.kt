111e5a7eaa052461c7350cc3d537f26fe5ae8944
kishanghosh090 <kishanghosh090@gmail.com>
2025-10-05 14:58:49 +0530

    add lamda fun

---
+++ second-phase/kotlinRiviosn/src/main/kotlin/adv/lamdaFun.kt
@@ -0,0 +1,31 @@
+package org.example.adv
+
+
+
+fun main(){
+    val input = readln()
+
+    val lambda: (Char) -> Boolean = {
+        it.isLetter()
+    }
+
+//    val lettersOnly = input.filter {it -> it.isLetter() }
+    var lettersOnly = input.filter (lambda)
+    println(lettersOnly)
+    println(lambda('c'))
+
+    lettersOnly = input.myFilter(lambda)
+
+    println(lettersOnly)
+}
+
+fun String.myFilter(predicate: (Char) -> Boolean) : String{
+
+    return buildString {
+        for (char in this@myFilter){
+            if(predicate(char)){
+                append(char)
+            }
+        }
+    }
+}
