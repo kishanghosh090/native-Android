e17d7bb9626eee640313b793ddde5f473ed819da
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-01 23:21:23 +0530

    add when expression

--- second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/NullOrNullity.kt
+++ second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/NullOrNullity.kt
@@ -10,4 +10,37 @@ fun main(){
 
     }
 
+
+
+    val myVal = readln().toIntOrNull() ?: 0
+
+
+    val final= when{
+        myVal % 2 == 0-> {
+            "even"
+        }
+        myVal %2 != 0 -> {
+            "odd"
+        }
+        else -> {
+            "no nothing"
+        }
+    }
+    val myVal2 = readln().toIntOrNull()
+    val finalS = when(myVal2){
+        null -> {
+            "null number"
+        }
+        2-> {
+            "even"
+        }
+        3 -> {
+            "odd"
+        }
+        else -> {
+            "no nothing"
+        }
+    }
+
+    println(finalS)
 }
