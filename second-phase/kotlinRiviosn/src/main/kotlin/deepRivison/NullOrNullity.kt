1d4df6131d691fa658d74587f038d10822836070
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-01 19:13:01 +0530

    add null

--- second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/NullOrNullity.kt
+++ second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/NullOrNullity.kt
@@ -3,8 +3,11 @@ package org.example.deepRivison
 fun main(){
     val input = readln()
 
-    val inputInt: Int = input.toIntOrNull() ?: -1
+    val inputInt = input.toIntOrNull()  // null --> 1,3,5,6..
 
     println(inputInt)
+    inputInt?.let {
+
+    }
 
 }
