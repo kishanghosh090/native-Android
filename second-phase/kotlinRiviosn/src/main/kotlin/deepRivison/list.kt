e87a2f6db13b89fa43ca81a3cbc43741e3f39fd8
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-02 02:07:03 +0530

    add arrays

---
+++ second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/list.kt
@@ -0,0 +1,12 @@
+package org.example.deepRivison
+
+fun main(){
+    val arrs = intArrayOf(1,2,3,4,5,6,7,77,7,7,7,7,7,4,4,3,2,43,5,6,7)
+    val favNums = listOf<Int>(1,2,3,4,5,6,7,8)
+    favNums.map { it1->
+        println(it1)
+    }
+    val nums = mutableListOf<Int>()
+    nums.add(12)
+
+}
