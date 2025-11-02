11dc1e0c81e24fe8cb03a5e47eb1f921af4e9bfb
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-02 11:05:42 +0530

    add string builder

--- second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/loops.kt
+++ second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/loops.kt
@@ -17,4 +17,12 @@ fun main(){
     for(i in mutableListOf<Int>(34,56,67,78,89) ){
         println(i)
     }
+
+
+    val finalString = buildString {
+        for (i in "kishan rana ghosh".lastIndex downTo 0){
+            append("$i ${when{i>0 -> "+" else -> ""}} ")
+        }
+    }
+    println(finalString)
 }
