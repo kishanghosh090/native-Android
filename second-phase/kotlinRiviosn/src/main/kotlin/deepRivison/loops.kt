a99790d980bf17ed150e3742aa8a126a661a97f7
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-02 11:00:38 +0530

    add loops until and downTo keyword

---
+++ second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/loops.kt
@@ -0,0 +1,20 @@
+package org.example.deepRivison
+
+fun main(){
+
+    for(i in 0..10){ // last number inclusive
+        println(i)
+    }
+
+    println("Enter a number to print it in console...")
+
+    val size = readln().toIntOrNull() ?: 0
+
+    for(i in 0 until  size){// exclusive the last range
+        println("$i")
+    }
+
+    for(i in mutableListOf<Int>(34,56,67,78,89) ){
+        println(i)
+    }
+}
