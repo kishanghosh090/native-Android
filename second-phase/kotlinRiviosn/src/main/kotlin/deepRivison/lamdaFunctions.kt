db710f9456bd242c57933b3c0d173b91b6b3dce7
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-04 01:09:08 +0530

    kotlin lamda

--- second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/lamdaFunctions.kt
+++ second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/lamdaFunctions.kt
@@ -3,8 +3,33 @@ package org.example.deepRivison
 fun main(){
     val input = readln()
 
-    input.map { value ->
-        println(value)
+    val letterOnly = input.map { value ->
+//        println(value)
+        value.isLetter()
     }
-    println(input)
+//    println(input)
+//    println(letterOnly)
+
+    val lamda: (Char) -> Boolean = {
+        it.isLetter()
+    }
+    val onlyLetter = input.filter(lamda)
+
+    val myFilter = input.myFilter { it ->
+        it.isLetter() || it == ' '
+    }
+    println(myFilter)
+
+
+}
+
+
+fun String.myFilter(predicate: (Char) -> Boolean): String {
+    return buildString {
+        for(i in this@myFilter){
+            if (predicate(i)){
+                append(i)
+            }
+        }
+    }
 }
