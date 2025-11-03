56773d24a17c592f5e11ad539ea8b1ef5193acab
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-03 20:38:47 +0530

    functions in kotlin

--- second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/functions.kt
+++ second-phase/kotlinRiviosn/src/main/kotlin/deepRivison/functions.kt
@@ -3,15 +3,24 @@ package org.example.deepRivison
 fun main(){
     println(myOwnFun("hello from kishan"))
     println(myOwnFun("hello from chai code..."))
+    println("hello from chai aur code".myReverseFun())
+
 }
 
 
-fun myOwnFun(s: String): String {
+fun String.myReverseFun(): String{
+    return buildString {
+        for (i in this@myReverseFun.lastIndex downTo 0){
+            append(this@myReverseFun[i])
+        }
+    }
+}
+
+fun myOwnFun(s: String = "default value"): String {
     println("hello from chai aur code")
     return  buildString{
         for (i in s.lastIndex downTo 0){
             append(s[i])
         }
     }
-
 }
